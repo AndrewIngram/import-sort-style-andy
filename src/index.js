02b06ff0b79fb070f2c15c0ddce73e2fcd7bd2a7
Andrew Ingram <andrewingram@andrews-mini.lan>
2020-12-09 13:05:50 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,152 @@
+const fixedOrder = ["react", "react-relay", "prop-types"];
+
+export default function(styleApi) {
+  const {
+    alias,
+    and,
+    not,
+    dotSegmentCount,
+    hasNoMember,
+    isAbsoluteModule,
+    isInstalledModule,
+    isNodeModule,
+    isRelativeModule,
+    moduleName,
+    unicode,
+    naturally,
+  } = styleApi;
+
+  const isReactEcosystemModule = (imported) =>
+    Boolean(
+      imported.moduleName.match(
+        /^(react|react-dom|react-native|react-relay|relay-runtime|prop-types|redux)/
+      )
+    );
+  const isArchitectureModule = (imported) =>
+    Boolean(imported.moduleName.match(/^(next|@emotion|date-fns)/));
+  const isFirstPartyModule = (imported) =>
+    Boolean(imported.moduleName.match(/^(~)/));
+  const isFirstPartyComponentModule = (imported) =>
+    Boolean(imported.moduleName.match(/^(~\/components)/));
+
+  const isStylesModule = (imported) =>
+    Boolean(imported.moduleName.match(/\.(s?css|less)$/));
+  const isImageModule = (imported) =>
+    Boolean(imported.moduleName.match(/\.(svg|png|gif|jpg|jpeg|webp)$/));
+
+  const reactComparator = (name1, name2) => {
+    let i1 = fixedOrder.indexOf(name1);
+    let i2 = fixedOrder.indexOf(name2);
+
+    i1 = i1 === -1 ? Number.MAX_SAFE_INTEGER : i1;
+    i2 = i2 === -1 ? Number.MAX_SAFE_INTEGER : i2;
+
+    return i1 === i2 ? naturally(name1, name2) : i1 - i2;
+  };
+
+  return [
+    // import "foo"
+    { match: and(hasNoMember, isAbsoluteModule) },
+    { separator: true },
+
+    // import "./foo"
+    {
+      match: and(
+        hasNoMember,
+        isRelativeModule,
+        not(isStylesModule),
+        not(isImageModule)
+      ),
+    },
+    { separator: true },
+
+    // import … from "fs";
+    {
+      match: isNodeModule,
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import React from "react";
+    {
+      match: isReactEcosystemModule,
+      sort: moduleName(reactComparator),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import { t } from '@lingui/macro';
+    {
+      match: isArchitectureModule,
+      sort: moduleName(reactComparator),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import uniq from 'lodash/uniq';
+    {
+      match: isInstalledModule(__filename),
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import { foo } from "~/util.js";
+    {
+      match: and(
+        isFirstPartyModule,
+        not(isFirstPartyComponentModule),
+        not(isStylesModule),
+        not(isImageModule)
+      ),
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import Component from "~/components/Component";
+    {
+      match: isFirstPartyComponentModule,
+      sort: moduleName(naturally),
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import … from "./foo";
+    // import … from "../foo";
+    {
+      match: and(isRelativeModule, not(isStylesModule), not(isImageModule)),
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import "./styles.css";
+    { match: and(hasNoMember, isRelativeModule, isStylesModule) },
+
+    // import styles from "./Components.scss";
+    {
+      match: isStylesModule,
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+
+    // import foo from "~/foo.jpg";
+    {
+      match: and(isFirstPartyModule, isImageModule),
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+    // import foo from "./foo.jpg";
+    {
+      match: isImageModule,
+      sort: [dotSegmentCount, moduleName(naturally)],
+      sortNamedMembers: alias(unicode),
+    },
+    { separator: true },
+    { separator: true },
+  ];
+}
